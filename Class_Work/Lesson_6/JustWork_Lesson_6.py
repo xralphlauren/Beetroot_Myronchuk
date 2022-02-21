9765cbad665d92f79c95a13b115498c17e84bc2a
xralphlauren <dsmyronchuk@gmail.com>
2022-02-21 21:09:15 +0200

    Home Work Fixed 5 HW

--- Class_Work/Lesson_6/JustWork_Lesson_6.py
+++ Class_Work/Lesson_6/JustWork_Lesson_6.py
@@ -49,3 +49,13 @@ print(a_dict.get('lst_1'))
 for i in a_dict.values():
     for y in i:
         print(y, type(y))
+
+
+x = 10
+for i in [1,2,3,4,5]:
+    if i % 2 == 0:
+        break
+    x -= i
+else:
+    x = 10
+print(x)
--- Home_Work/Lesson_5/task3.py
+++ Home_Work/Lesson_5/task3.py
@@ -1,3 +1,4 @@
+# Old variant
 import random
 
 lst_1 = []
@@ -14,3 +15,20 @@ while i < len(lst_1):
     i += 1
 
 print(lst_2)
+
+# new variant
+
+v2_lst_1 = []
+v2_lst_2 = []
+i = 1
+while i < 100:
+    v2_lst_1.append(i)
+    i += 1
+
+i = 0
+while i < len(v2_lst_1):
+    if v2_lst_1[i] % 7 == 0 and v2_lst_1[i] % 5 != 0:
+        v2_lst_2.append(v2_lst_1[i])
+    i += 1
+
+print(v2_lst_2)
